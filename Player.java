aa60eb1cc5bf836d17926eb236c1f7ee5f421293
Tom <tom9383@gmail.com>
2013-11-22 11:15:21 +0000

    Update Player.java
    Eliminated the use of player-specific houses+stores array list.

--- Player.java
+++ Player.java
@@ -1,55 +1,36 @@
-import java.util.ArrayList;
-
 public class Player {
 
-    private int id;
     private boolean turn;
-    private int chosen;
-    private int target;
+    private int     chosen;
+    private int     target;
 
-    private ArrayList<House> houses;
-
-    public Player(int i) {
-        this.id     = i;
-        this.houses = new ArrayList<House>();
+    public Player() {
         this.turn   = false;
+        this.chosen = 0;
         this.target = 0;
-
-        House h1    = new House(0, 4);
-        House h2    = new House(1, 4);
-        House h3    = new House(2, 4);
-        House h4    = new House(3, 4);
-        House h5    = new House(4, 4);
-        House h6    = new House(5, 4);
-        House store = new House(6, 0);
-
-        this.houses.add(h1);
-        this.houses.add(h2);
-        this.houses.add(h3);
-        this.houses.add(h4);
-        this.houses.add(h5);
-        this.houses.add(h6);
-        this.houses.add(store);
     }
 
     public void setTurn(boolean b) {
         this.turn = b;
     }
 
-    public boolean isTurn() {
+    public boolean getTurn() {
         return this.turn;
     }
 
-    public void select(int i) {
+    public void setChosen(int i) {
         this.chosen = i;
-        this.target = this.chosen + this.houses.get(this.chosen).getBeanNum();
+    }
+
+    public int getChosen() {
+        return this.chosen;
+    }
+
+    public void setTarget(int i) {
+        this.target = i;
     }
 
     public int getTarget() {
         return this.target;
-    }
-
-    public House getHouse(int i) {
-        return this.houses.get(i);
     }
 }
